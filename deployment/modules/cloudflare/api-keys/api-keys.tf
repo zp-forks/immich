4ed7aa33263a2cf439f78c30eee7065f8ded6ffe
Zack Pollard <zackpollard@ymail.com>
2024-05-28 16:48:07 +0100

    feat: terraform for managing cloudflare account

---
+++ deployment/modules/cloudflare/api-keys/api-keys.tf
@@ -0,0 +1,40 @@
+data "cloudflare_api_token_permission_groups" "all" {}
+
+resource "cloudflare_api_token" "terraform_cloudflare_account" {
+  name        = "terraform"
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.account["Pages Write"],
+      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
+      data.cloudflare_api_token_permission_groups.all.zone["Zone Write"],
+      data.cloudflare_api_token_permission_groups.all.zone["Zone Settings Write"],
+      data.cloudflare_api_token_permission_groups.all.zone["Dynamic URL Redirects Write"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.*" = "*"
+    }
+  }
+}
+
+output "terraform_key_cloudflare_account" {
+  value = cloudflare_api_token.terraform_cloudflare_account.value
+  sensitive = true
+}
+
+resource "cloudflare_api_token" "terraform_cloudflare_docs" {
+  name        = "terraform"
+  policy {
+    permission_groups = [
+      data.cloudflare_api_token_permission_groups.all.account["Pages Write"],
+      data.cloudflare_api_token_permission_groups.all.zone["DNS Write"],
+    ]
+    resources = {
+      "com.cloudflare.api.account.*" = "*"
+    }
+  }
+}
+
+output "terraform_key_cloudflare_docs" {
+  value = cloudflare_api_token.terraform_cloudflare_account.value
+  sensitive = true
+}
